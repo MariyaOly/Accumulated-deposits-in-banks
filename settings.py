aa99b6d6c17d8642632909b07f55faaf7e63d098
Mariya Olyushkina <125290632+MariyaOly@users.noreply.github.com>
2023-06-13 11:14:21 +0200

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-per_cent = {'BBVA': 5.6, 'ING': 5.9, 'BDE': 4.28, 'CAIXA': 4.0}
-money = int(input("Enter the amount you plan to deposit at interest: "))
-deposit = [int(money * rate / 100) for rate in per_cent.values()]
-max_deposit = max(deposit)
-print("Deposits in banks:", deposit)
-print("The maximum amount you can earn is:", max_deposit)
